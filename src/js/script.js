339b7620842022ef308507832a20463bd8bf181a
Isaac Celso <isaac.celsoa@gmail.com>
2023-05-06 14:48:32 -0300

    first commit

---
+++ src/js/script.js
@@ -0,0 +1,19 @@
+function novaCuriosidade() {
+  // carrega o arquivo txt
+  var xhr = new XMLHttpRequest();
+  xhr.open("GET", "/src/curiosidades/gerais/curiosidades.txt", true);
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState == 4 && xhr.status == 200) {
+      var linhas = xhr.responseText.split("\n");
+      var linha = linhas[Math.floor(Math.random() * linhas.length)];
+
+      var curiosidadeDiv = document.querySelector(".curiosidade");
+      curiosidadeDiv.innerText = linha;
+    }
+  };
+  xhr.send();
+}
+
+window.addEventListener("load", function () {
+  novaCuriosidade();
+});
